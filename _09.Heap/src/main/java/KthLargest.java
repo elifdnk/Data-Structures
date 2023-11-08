48b226f13c7131f6627c2205d62b516b6ba34118
ELİF DENK <elifdenkk@gmail.com>
2023-11-08 20:15:14 +0300

    some commands added

--- _09.Heap/src/main/java/KthLargest.java
+++ _09.Heap/src/main/java/KthLargest.java
@@ -6,12 +6,15 @@ public class KthLargest {
     public static int getKthLargest(int[] array, int k){
         // create a heap
         MyHeap heap=new MyHeap(array.length);
+        //Put array into a heap
         for (int i = 0; i < array.length; i++) {
           heap.insert(array[i]);
         }
+        ///remove root k-1 times
         for (int i = 0; i <k-1 ; i++) {
             heap.remove();
         }
+        //return root
         return heap.peek();
     }
 }
