880ec3951a8a20e024c33f1b49973c5b3f750d3f
ELİF DENK <elifdenkk@gmail.com>
2023-11-18 13:51:52 +0300

    ExponentialSearch

---
+++ _10.SearchAlgorithms/src/main/java/ExponentialSearch.java
@@ -0,0 +1,18 @@
+public class ExponentialSearch {
+    public static void main(String[] args) {
+        int[] numbers=new int[30];
+        for (int i = 0; i < 30; i++) {
+            numbers[i]=i;
+        }
+        System.out.println(exponentialSearch(numbers, 23));
+
+    }
+    public static int exponentialSearch(int[] array, int data){
+        int bound=1;
+        while (bound<array.length && array[bound]<data) bound*=2;
+
+        int left=bound/2;
+        int right=Math.min(bound, array.length-1);
+        return BinarySearch.binarySearchRecursive(array, data, left,right);
+    }
+}
